56cd96714aeddc21b742c78ae2131e3182093b9f
Ihor Kalyniak <kalyniak.ihor@gmail.com>
2021-07-25 20:29:57 +0300

    added ButtonScrollUp

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './sass/main.scss';
 import './js/fetchAPI.js';
 import './js/localStorageAPI.js';
+import './js/buttonScrollUp.js';
 import aboutMovieTemplates from './templates/aboutMovieTemplates.hbs';
-import movieCardTemplate from './templates/movieCardTemplate.hbs'
+import movieCardTemplate from './templates/movieCardTemplate.hbs';
