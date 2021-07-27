c2277702fe53f8f5627771e8e85b3e30044b3b95
bogdankol <bogdank208@gmail.com>
2021-07-27 15:21:36 +0300

    added api actions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './js/spinner.js';
 import './sass/main.scss';
-import './js/fetchAPI.js';
+import Fetch from './js/fetchAPI.js';
 import './js/localStorageAPI.js';
 import './js/buttonScrollUp.js';
 import aboutMovieTemplates from './templates/aboutMovieTemplates.hbs';
