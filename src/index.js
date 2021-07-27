686a64649d177688371d1e60dcc93ceb14b14abb
Petryk Roman <romic.argument@gmail.com>
2021-07-27 16:14:15 +0300

    Додає стилі для карточок галереї

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import './sass/main.scss';
 import './js/fetchAPI.js';
 import './js/localStorageAPI.js';
 import aboutMovieTemplates from './templates/aboutMovieTemplates.hbs';
-import movieCardTemplate from './templates/movieCardTemplate.hbs'
+import movieCardTemplate from './templates/movieCardTemplate.hbs';
