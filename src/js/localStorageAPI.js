0d35bee74c803266bee8ba08ed210327cbd06932
Anzhela Boiko <boikoanzhela@gmail.com>
2021-07-27 16:13:29 +0300

    create set, delete methods

---
+++ src/js/localStorageAPI.js
@@ -0,0 +1,17 @@
+let watchedMovies = [];
+let queueMovies = [];
+
+class localStorageAPI {
+  constructor() {}
+
+  static set(movieObj, listArr, key) {
+    listArr.push(movieObj);
+    localStorage.setItem(key, JSON.stringify(listArr));
+  }
+
+  static delete(movieObj, listArr, key) {
+    const movieIndex = listArr.indexOf(movieObj);
+    listArr.splice(movieIndex, 1);
+    localStorage.setItem(key, JSON.stringify(listArr));
+  }
+}
