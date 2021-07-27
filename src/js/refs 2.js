c006519b4e58ebbe4adb31ab7e3ca88872e586dd
Dima <75967292+DukaDima@users.noreply.github.com>
2021-07-28 00:24:56 +0300

    created refs-object

---
+++ src/js/refs 2.js
@@ -0,0 +1,12 @@
+const refs = {
+  searchInput: document.querySelector('.search-control'),
+  linkMyLibrary: document.querySelector('.navigation-menu-link'),
+  linkHome: document.querySelector('.navigation-menu-link'),
+  logoHome: document.querySelector('.logo'),
+  aboutTeam: document.querySelector('.link-footer'),
+};
+console.log(refs.aboutTeam);
+console.log(refs.searchInput);
+console.log(refs.myLibrary);
+console.log(refs.linkHome);
+console.log(refs.logoHome);
---
+++ src/js/refs.js
@@ -0,0 +1,22 @@
+export const refs = {
+  searchInput: document.querySelector('.search-control'),
+  linkMyLibrary: document.getElementById('lib_page'),
+  linkHome: document.getElementById('home_page'),
+  logoHome: document.querySelector('.logo'),
+  watchedLibrary: document.querySelector('.btn_watched'),
+  queueLibrary: document.querySelector('.btn_queue'),
+  aboutTeam: document.querySelector('.link-footer'),
+  galleryList: document.querySelector('.gallery-list'),
+  btnScrollUP: document.querySelector('.js-buttonScrollUp'),
+  spinnerPreloader: document.getElementById('preloader-page'),
+  modalBtnRemoveWatched: document.querySelector('.modal-btn-remove-watched'),
+  modalBtnWatched: document.querySelector('.modal-btn-watched'),
+  modalBtnQueue: document.querySelector('.modal-btn-queue'),
+  modalBtnRemoveQueue: document.querySelector('.modal-btn-remove-queue'),
+
+  modalBackdrop: document.querySelector('.backdrop-js'),
+  modalCloseBtn: document.querySelector('.js-close-btn'),
+};
+
+console.log(refs.modalBackdrop);
+console.log(refs.modalCloseBtn);
