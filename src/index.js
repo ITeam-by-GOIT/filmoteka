d0374eff18f9fe67e830f1a4b00208bcc10f4a56
Dima <75967292+DukaDima@users.noreply.github.com>
2021-07-28 09:24:15 +0300

    create infinite scroll

--- src/index.js
+++ src/index.js
@@ -7,3 +7,4 @@ import './js/localStorageAPI.js';
 import './js/buttonScrollUp.js';
 import aboutMovieTemplates from './templates/aboutMovieTemplates.hbs';
 import movieCardTemplate from './templates/movieCardTemplate.hbs';
+import './js/infiniteScroll.js';
---
+++ src/js/infiniteScroll.js
@@ -0,0 +1,19 @@
+//--------------------------бесконечный скролл-------------------------
+
+window.addEventListener('scroll', onScroll);
+// в функцию onScroll прокидывается соответствующий вызов await fetch в качестве первого параметра и
+// соответствующий рендеринг в качестве второго параметра
+
+async function onScroll(fetch, render) {
+  const documentRect = document.documentElement.getBoundingClientRect();
+
+  if (documentRect.bottom <= document.documentElement.clientHeight + 150) {
+    pageCounter += 1;
+
+    let resultList = await fetch;
+    render(resultList);
+    // let resultList = await imageFetcher.fetchImages(searchQuery, pageCounter);
+    // renderGallery(resultList);
+  }
+}
+//--------------------------------------------------------
