dd4dc1e4229542e71c3b2dd291c571e9271885f4
Anastasia <nastasia199126@gmail.com>
2021-07-28 21:09:45 +0300

    markup

---
+++ src/js/modal-movi-render.js
@@ -0,0 +1,15 @@
+import movieDetailsCard from '../templates/movieCardTemplate.hbs';
+import NewFetchApi from './fetchAPI';
+
+const newFetchApi = new NewFetchApi();
+const wrapper = document.querySelector('.modal-wrapper');
+
+async function renderMovieDetails() {
+  const response = await newFetchApi.searchByMovieId(76857);
+  console.log(response);
+  const markup = movieDetailsCard(response);
+  console.log(markup);
+  wrapper.insertAdjacentHTML('beforeend', markup);
+}
+
+renderMovieDetails();
