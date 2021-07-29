3ab51a905815163779af4da9f3d0caa6d2d4b44f
Ihor Kalyniak <kalyniak.ihor@gmail.com>
2021-07-29 04:32:48 +0300

    toastr.error

--- src/index.js
+++ src/index.js
@@ -10,5 +10,11 @@ import aboutMovieTemplates from './templates/aboutMovieTemplates.hbs';
 import movieCardTemplate from './templates/movieCardTemplate.hbs';
 import onSubmitHandler from './js/eventListenerSearch.js';
 
-
 refs.searchForm.addEventListener('submit', onSubmitHandler);
+
+// ------------ for test notification (for example, with an invalid key)
+import FetchAPI from './js/fetchAPI.js';
+const fetchAPI = new FetchAPI();
+const promise = fetchAPI.searchByTrending('day', 67);
+promise.then(data => console.log(data.results[0].release_date.slice(0, 4)));
+// ------------ the end
--- src/js/fetchAPI.js
+++ src/js/fetchAPI.js
@@ -1,3 +1,24 @@
+import toastr from 'toastr';
+import 'toastr/build/toastr.css';
+
+toastr.options = {
+  closeButton: true,
+  debug: true,
+  newestOnTop: false,
+  progressBar: true,
+  positionClass: 'toast-top-center',
+  preventDuplicates: false,
+  onclick: null,
+  showDuration: '300',
+  hideDuration: '1000',
+  timeOut: '5000',
+  extendedTimeOut: '1000',
+  showEasing: 'swing',
+  hideEasing: 'linear',
+  showMethod: 'fadeIn',
+  hideMethod: 'fadeOut',
+};
+
 export default class FetchAPI {
   constructor() {
     this.API_KEY = '05b27f765345223aac972c2dbb5eec37';
@@ -10,6 +31,7 @@ export default class FetchAPI {
     if (response.ok) {
       return await response.json();
     }
+    toastr.error('Unsuccessful results. Try again!');
     throw new Error(await response.text());
   }
 
@@ -21,6 +43,7 @@ export default class FetchAPI {
     if (response.ok) {
       return await response.json();
     }
+    toastr.error('Unsuccessful results. Try again!');
     throw new Error(await response.text());
   }
 
@@ -32,6 +55,7 @@ export default class FetchAPI {
     if (response.ok) {
       return await response.json();
     }
+    toastr.error('Unsuccessful results. Try again!');
     throw new Error(await response.text());
   }
 
@@ -43,7 +67,7 @@ export default class FetchAPI {
     if (response.ok) {
       return await response.json();
     }
+    toastr.error('Unsuccessful results. Try again!');
     throw new Error(await response.text());
   }
 }
-
