8baa7ffd52c77041aa6189156d82054c5beecdf7
koliadjun <koliadjun@gmail.com>
2021-07-29 22:19:20 +0300

    fixed

--- src/js/eventListenerGetDataFromLocalStorage.js
+++ src/js/eventListenerGetDataFromLocalStorage.js
@@ -1,8 +1,8 @@
 import { localStorageAPI } from './localStorageAPI.js'
 export default function onWatchedHandler(key) {
     const data = localStorageAPI.get(key);
-    if (data.length === 0) {
-        return
+    if (!data || data.length === 0) {
+        return 1;
     }
 
     return data;
