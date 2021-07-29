22f74e4c49399d31c4d846fe3dbc374b0d613204
Anastasia <nastasia199126@gmail.com>
2021-07-29 19:50:06 +0300

    modal

--- src/js/modal-movi-render.js
+++ src/js/modal-movi-render.js
@@ -2,7 +2,7 @@ import movieDetailsCard from '../templates/aboutMovieTemplates.hbs';
 import NewFetchApi from './fetchAPI';
 
 const newFetchApi = new NewFetchApi();
-const wrapper = document.querySelector('.modal-wrapper');
+const wrapper = document.querySelector('.modal');
 
 async function renderMovieDetails() {
   const response = await newFetchApi.searchByMovieId(76857);
