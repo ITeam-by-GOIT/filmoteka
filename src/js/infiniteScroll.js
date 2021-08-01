cf3196640909a4eac8a22ddd8be9f7ebdcb6e06e
koliadjun <koliadjun@gmail.com>
2021-08-01 22:57:37 +0300

    clear console.log

--- src/js/infiniteScroll.js
+++ src/js/infiniteScroll.js
@@ -4,11 +4,6 @@ import { refs } from './refs';
 import { renederGalleryMarckUp, createGenres, createCardYear } from './renderGallery.js';
 import { spinnerMethod } from './spinner';
 const debounce = require('lodash.debounce');
-console.log(refs);
-
-console.log(spinnerMethod);
-console.log(spinnerMethod.removeSpinner);
-console.log(spinnerMethod.addSpinner);
 
 const fetch = new FetchAPI();
 // const footer = document.querySelector('.footer-container');
@@ -22,25 +17,15 @@ observer.observe(refs.aboutTeam);
 async function onRender(entries) {
   if (entries[0].isIntersecting) {
     spinnerMethod.addSpinner();
-
-    // spinner.classList.remove('preloader-js');
-
     if (refs.galleryList.children.length !== 0) {
       page += 1;
-      // spinner();
-      // console.log(spinner);
-
       let resultList = await fetch.searchByTrending(undefined, page); //запрос следующей страницы
-      console.log(resultList);
-
       if (page > resultList.total_pages) {
         spinnerMethod.removeSpinner();
-        // spinner.classList.add('preloader-js');
         return;
       } else {
         render(resultList.results);
         spinnerMethod.removeSpinner();
-        // spinner.classList.add('preloader-js');
       }
     } else {
       return;
@@ -50,23 +35,18 @@ async function onRender(entries) {
 
 async function render(data) {
   spinnerMethod.addSpinner();
-  // spinner.classList.remove('preloader-js');
   try {
     const genres = await fetch.getGenres().then(list => {
       return list.genres;
     });
     const result = await renederGalleryMarckUp(data, genres);
-
     refs.galleryList.insertAdjacentHTML('beforeend', movieCardTemplate(result));
-
     const img = document.querySelectorAll('.js-card-img');
     if (refs.galleryList.lastElementChild.complete) {
       spinnerMethod.removeSpinner();
-      // spinner.classList.add('preloader-js');
     }
   } catch (e) {
     console.log("Opps we got some error here...don't panic! we already did it for you :)", e);
     spinnerMethod.removeSpinner();
-    // spinner.classList.add('preloader-js');
   }
 }
