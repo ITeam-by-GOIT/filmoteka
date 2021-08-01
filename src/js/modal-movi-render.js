f7d148dcba5755d60e053cd92b78e51851ab7c61
Anastasia <nastasia199126@gmail.com>
2021-08-01 18:27:57 +0300

    modal

--- src/js/modal-movi-render.js
+++ src/js/modal-movi-render.js
@@ -5,18 +5,31 @@ const newFetchApi = new NewFetchApi();
 const wrapper = document.querySelector('.modal');
 
 async function renderMovieDetails() {
-  const response = await newFetchApi.searchByMovieId(76857);
-  console.log(response);
+  const response = await newFetchApi.searchByMovieId(1256);
+
   const markup = movieDetailsCard(response);
-  console.log(markup);
+
   wrapper.insertAdjacentHTML('beforeend', markup);
+
+  const imgPlay = document.querySelector('.modal-img-play');
+  console.log(imgPlay);
+  if (imgPlay) {
+    imgPlay.addEventListener('click', function (e) {
+      e.preventDefault();
+      const html =
+        '<div class="backdrop js-backdrop"><div class="modal"><button type="button" class="modal-close-btn js-close-btn"><svg class="modal-close-btn__icon" width="14" height="14"><use href="./images/sprite.svg#icon-close"></use></svg></button></div></div>';
+      if (e.target.classList.contains('modal-img-play')) {
+        console.log('hgkghdg');
+        document.querySelector('BODY').innerHTML = html;
+        // insertAdjacentHTML('beforeend', html);
+        // body.insertAdjacentHTML('beforeend', html);
+      }
+    });
+  }
 }
 
 renderMovieDetails();
 
-const div = document.querySelector('.overlay');
-const image = document.createElement('img');
-image.setAttribute('src', '../images/play.png');
-image.setAttribute('alt', 'play');
-div.innerHTML = image;
-//  class="modal-img-play" src = "../images/play.png" alt = "icon play" >
+// const treiler = newFetchApi.searchByMovieId(1256);
+
+// console.log(treiler.video);
