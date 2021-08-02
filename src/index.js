14f62930f9fa6892950b22b8016e22d4ce0030b7
koliadjun <koliadjun@gmail.com>
2021-08-02 03:19:20 +0300

    change link order

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import './js/headerMenu.js';
 import './js/spinner.js';
 import './sass/main.scss';
+import './js/infiniteScroll.js';
 import './js/fetchAPI.js';
 import './js/localStorageAPI.js';
 import './js/buttonScrollUp.js';
 // import './js/modal-movie-render.js';
-import './js/infiniteScroll.js';
 import './js/modalClose.js';
 import './js/onCardClick.js';
 import './js/renderFromLocalStorage.js';
