785e9d0bc2488e9ea55f62e47352cc15cf94d0c4
koliadjun <koliadjun@gmail.com>
2021-08-02 03:20:52 +0300

    chache lib render

--- src/js/headerMenu.js
+++ src/js/headerMenu.js
@@ -1,22 +1,29 @@
 import { refs } from './refs.js';
+import { renderMovieList } from './renderFromLocalStorage.js';
+import { renderTrending } from './renderGallery.js';
+import { localStorageAPI } from './localStorageAPI.js';
 
 // /*=== Перемикання сторінок з використанням класів ===*/
 
 const controlPageHome = function () {
+  refs.galleryList.innerHTML = '';
   refs.linkMyLibrary.classList.remove('current');
   refs.linkHome.classList.add('current');
   refs.header.classList.remove('header-container_library');
   refs.header.classList.add('header-container_home');
   refs.searchForm.classList.remove('visually-hidden');
   refs.headerButtons.classList.add('visually-hidden');
+  renderTrending();
 };
 const controlPageLib = function () {
+  refs.galleryList.innerHTML = '';
   refs.linkHome.classList.remove('current');
   refs.linkMyLibrary.classList.add('current');
   refs.header.classList.remove('header-container_home');
   refs.header.classList.add('header-container_library');
   refs.headerButtons.classList.remove('visually-hidden');
   refs.searchForm.classList.add('visually-hidden');
+  renderMovieList(localStorageAPI.KEYS.WATCHED, 1);
 };
 refs.linkMyLibrary.addEventListener('click', controlPageLib);
 refs.linkHome.addEventListener('click', controlPageHome);
@@ -42,5 +49,6 @@ for (const button of buttons) {
   button.addEventListener('click', function () {
     buttons.forEach(i => i.classList.remove('active-btn'));
     this.classList.toggle('active-btn');
+    renderMovieList(this.dataset.libtype, 1);
   });
 }
