41bf4a6229a7ce87d7ec55833f369fde3d6b3529
koliadjun <koliadjun@gmail.com>
2021-08-02 11:29:07 +0300

    add info team

--- src/js/team-modal.js
+++ src/js/team-modal.js
@@ -3,23 +3,76 @@ import teamModalTemplate from '../templates/teamModalTemplate.hbs';
 
 const team = [
     {
-        name: "Yuriy",
-        lastName: "Koliadzhyn",
+        name: "Anzhela",
+        lastName: "Boiko",
         description: "Hello world!",
-        instagramLink: "https://www.instagram.com/iurakoliadzhin/",
         githubLink: "https://github.com/Koliadjun",
-        facebookLink: "https://github.com/Koliadjun",
+        linkedInLink: "https://github.com/Koliadjun",
+    },
+    {
+        name: "Anastasia",
+        lastName: "",
+        description: "Hello world!",
+        githubLink: "https://github.com/Koliadjun",
+        linkedInLink: "https://github.com/Koliadjun",
+    },
+    {
+        name: "Bogdan",
+        lastName: "Kolesnikov",
+        description: "Hello world!",
+        githubLink: "https://github.com/Koliadjun",
         linkedInLink: "https://github.com/Koliadjun",
     },
     {
         name: "Dima",
         lastName: "Dyka",
         description: "Hello world!",
-        instagramLink: "https://www.instagram.com/iurakoliadzhin/",
         githubLink: "https://github.com/Koliadjun",
-        facebookLink: "https://github.com/Koliadjun",
         linkedInLink: "https://github.com/Koliadjun",
-    }
+    },
+    {
+        name: "Ihor",
+        lastName: "Kalyniak",
+        description: "Hello world!",
+        githubLink: "https://github.com/Koliadjun",
+        linkedInLink: "https://github.com/Koliadjun",
+    },
+    {
+        name: "Olga",
+        lastName: "Kostina",
+        img: "https://media-exp1.licdn.com/dms/image/C5603AQGikyj3n0zY2w/profile-displayphoto-shrink_800_800/0/1517587157103?e=1633564800&v=beta&t=3IgApPVxqXJ4J36t83rsJao6zk49GUB0iTEK11IM-T4",
+        description: "Hello world!",
+        githubLink: "https://github.com/Koliadjun",
+        linkedInLink: "https://github.com/Koliadjun",
+    },
+    {
+        name: "Roma",
+        lastName: "Petryk",
+        description: "Hello world!",
+        githubLink: "https://github.com/Koliadjun",
+        linkedInLink: "https://github.com/Koliadjun",
+    },
+    {
+        name: "Vasyl",
+        lastName: "Shcherbyna",
+        description: "Hello world!",
+        githubLink: "https://github.com/Koliadjun",
+        linkedInLink: "https://github.com/Koliadjun",
+    },
+    {
+        name: "Євгенія",
+        lastName: "",
+        description: "Hello world!",
+        githubLink: "https://github.com/Yevheniia87",
+        linkedInLink: "https://github.com/Koliadjun",
+    },
+    {
+        name: "Yuriy",
+        lastName: "Koliadzhyn",
+        description: "Hello world!",
+        githubLink: "https://github.com/Koliadjun",
+        linkedInLink: "https://github.com/Koliadjun",
+    },
 ];
 const showTeamModal = (e) => {
     e.preventDefault();
