6c06787c57a0a5ceab223cf404889f6cb29efb16
koliadjun <koliadjun@gmail.com>
2021-08-03 16:27:09 +0300

    import file for trailer

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './js/infiniteScroll.js';
 import './js/fetchAPI.js';
 import './js/localStorageAPI.js';
 import './js/buttonScrollUp.js';
-// import './js/modal-movie-render.js';
+import './js/trailer.js';
 import './js/modalClose.js';
 import './js/onCardClick.js';
 import './js/renderFromLocalStorage.js';
